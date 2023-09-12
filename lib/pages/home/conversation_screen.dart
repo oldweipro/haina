f80e64e1054b1aa2f176feafb7951b7138783ac4
oldweipro <oldweipro@gmail.com>
2023-09-12 23:57:51 +0800

    update

--- lib/pages/home/conversation_screen.dart
+++ lib/pages/home/conversation_screen.dart
@@ -1,11 +1,41 @@
 import 'package:flutter/material.dart';
-import 'package:haina/pages/sign_in/page.dart';
+import 'package:get/get.dart';
+import 'package:haina/common/router/router.dart';
+import 'package:haina/common/store/store.dart';
 
 class ConversationScreen extends StatelessWidget {
   const ConversationScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const SignInPage();
+    return Expanded(
+      child: ListView(
+        children: <Widget>[
+          const SizedBox(height: 7),
+          ElevatedButton(
+            onPressed: () {
+              Get.toNamed<void>(AppRoutes.setting);
+            },
+            child: const Text("按钮"),
+          ),
+          const SizedBox(height: 7),
+          const Text("😂哈哈哈"),
+          const SizedBox(height: 7),
+          ElevatedButton(
+            onPressed: () {
+              Get.toNamed(AppRoutes.chat);
+              // Get.toNamed(AppRoutes.chatOld);
+            },
+            child: const Text("聊天页面"),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              UserStore.to.onLogout();
+            },
+            child: const Text("退出登陆"),
+          ),
+        ],
+      ),
+    );
   }
 }
